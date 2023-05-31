04aeb9552e897a74290ca62250fe60a2d074eece
Diegu <diegudeveloper@gmail.com>
2023-05-31 10:44:37 +0200

    configuracion y creacion del objeto y su llenado por cada selecion de las caracteristicas.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 //variables
-const resultado = document.querySelector("#resultado"); 
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+const resultado = document.querySelector("#resultado"); 
 
 const max = new Date().getFullYear(); 
 const min = max - 10;
 // console.log(min);
 
 
+//generar un objeto
+
+const datosBusqueda = {
+    marca: "",
+    year: "",
+    minimo: "",
+    maximo: "",
+    puertas: "",
+    transmision: "",
+    color: "",
+}
 
 
 
@@ -17,8 +35,30 @@ document.addEventListener("DOMContentLoaded", () => {
     //llenar las opciones del años.
     llenarSelect();
 
-
 });
+
+marca.addEventListener("change", (e) => {
+    datosBusqueda.marca = e.target.value;
+})
+year.addEventListener("change", (e) => {
+    datosBusqueda.year = e.target.value;
+})
+minimo.addEventListener("change", (e) => {
+    datosBusqueda.minimo = e.target.value;
+})
+maximo.addEventListener("change", (e) => {
+    datosBusqueda.maximo = e.target.value;
+})
+puertas.addEventListener("change", (e) => {
+    datosBusqueda.puertas = e.target.value;
+})
+transmision.addEventListener("change", (e) => {
+    datosBusqueda.transmision = e.target.value;
+})
+color.addEventListener("change", (e) => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
 
 
 
